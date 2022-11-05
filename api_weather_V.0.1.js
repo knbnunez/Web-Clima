6f025e803df75c1ec3e32b3d7cef66a5c60acef6
knbnunez <knbnunez@gmail.com>
2022-11-05 11:07:46 -0300

    recorrido v2

--- api_weather_V.0.1.js
+++ api_weather_V.0.1.js
@@ -19,6 +19,15 @@ function imprimir(idx, hora, temp, precip) {
     return fila;
 }
 
+function formatearPrecipitacion(precipConsulta) {
+    if (precipConsulta < 2) return "lluvias débiles";
+    else if (precipConsulta <= 15) return "lluvia";
+    else if (precipConsulta <= 30) return "lluvias fuertes"; 
+    else if (precipConsulta <= 60) return "lluvias muy fuertes";
+    else return "lluvias torrenciales";
+}
+
+
 var fechaActual = new Date();
 fechaActual = moment(fechaActual).format('YYYY-MM-DD\THH:00');
 
@@ -30,16 +39,28 @@ function recorrerAPI() {
             if (clima.hourly.time[idx] >= fechaActual) {
                 // Obtener los datos útiles de la consulta
                 let fechaConsulta = clima.hourly.time[idx];
-                let tempConsulta = clima.hourly.apparent_temperature[idx];
+                // Esta es la sesación térmica
+                let sensacionTConsulta = clima.hourly.apparent_temperature[idx];
                 let precipConsulta = clima.hourly.precipitation[idx];
-                
+                // Falta velocidad del viento
+                let velovidadVConsulta = clima.hourly.windspeed_10m[idx];
+                // Falta la temperatura ambiente
+                let temperaturaAConsulta = clima.hourly.temperature_2m[idx];
+
                 // Formatearlos para la vista
-                let horaActual = moment(fechaActual).format('HH:mm');
                 fechaConsulta = moment(fechaConsulta).format('MM-DD HH:mm');
+                precipitacion = formatearPrecipitacion(precipConsulta);
 
                 // 1° caso
-                if (clima.hourly.time[idx] == fechaActual) climaActual.innerHTML += imprimir(0, horaActual, tempConsulta, precipConsulta);
-                else climasPosteriores.innerHTML += imprimir(idx, fechaConsulta, tempConsulta, precipConsulta);
+                if (clima.hourly.time[idx] == fechaActual) {
+                    // Formateo
+                    let fechaActual = new Date();
+                    let horaActual = moment(fechaActual).format('HH:mm');
+                    
+                    // Impresion
+                    climaActual.innerHTML += imprimir(0, horaActual, tempConsulta, precipitacion);
+                }
+                else climasPosteriores.innerHTML += imprimir(idx, fechaConsulta, tempConsulta, precipitacion);
             }
         }) 
     });
