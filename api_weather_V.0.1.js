2adf57df4caf78206ac90f7460eb6c753c47959f
knbnunez <106483091+knbnunez@users.noreply.github.com>
2022-11-05 10:07:52 -0300

    Add files via upload

--- api_weather_V.0.1.js
+++ api_weather_V.0.1.js
@@ -1,16 +1,13 @@
-// const moment = require("moment/moment");
-
 const apiWeather = 'https://api.open-meteo.com/v1/forecast?latitude=-54.82&longitude=-68.36&hourly=temperature_2m,apparent_temperature,precipitation,windspeed_10m&timezone=America%2FSao_Paulo';
 
-//
+// Datos sin procesar
 fetch(apiWeather)
     .then(response => response.json())
     .then(json => {
-        console.log('Data original: '+json);
+        console.log(json);
     });
 
-
-function listado(idx, hora, temp, precip) {
+function imprimir(idx, hora, temp, precip) {
     let fila = `
         <tr class="filaTabla">              <!-- Puedo agregarle CSS a ésta clase de acá -->
             <th scope="row">${idx}</th>
@@ -22,70 +19,28 @@ function listado(idx, hora, temp, precip) {
     return fila;
 }
 
-// Obtiene la hora actual. 
-// Formatea al mismo formato fecha de la API
-// Filtra la hora de la API que coincida con la hora actual
-// Muestra la data referente a la hora actual
 var fechaActual = new Date();
 fechaActual = moment(fechaActual).format('YYYY-MM-DD\THH:00');
-// console.log(fechaActual);
-
-function imprimirActual() {
-    fetch(apiWeather)
-    .then(response => response.json()) 
-    .then(clima => {
-        let fechaActualAPI = clima.hourly.time.filter(fechaActualAPI => fechaActualAPI == fechaActual);
-        console.log(fechaActualAPI);
-        let horaActual = moment(fechaActualAPI).format('MM-DD HH:mm');
-        console.log(horaActual);
-        let tempActual = clima.hourly.apparent_temperature.shift();
-        let precipActual = clima.hourly.precipitation.shift();
-        // console.log(horaActual, tempActual, precipActual);
-        climaActual.innerHTML += listado(0, horaActual, tempActual, precipActual);
-    });
-}
-
-
-// Imprime solo los datos posteriores a la hora y fecha actual
-function imprimirPosteriores() {
-    fetch(apiWeather)
-    .then(response => response.json()) 
-    .then(clima => { 
-        clima.hourly.time.forEach((dato, idx) => {
-            if (clima.hourly.time[idx] > fechaActual) {
-                let horaPost = clima.hourly.time.shift();
-                horaPost = moment(horaPost).format('MM-DD HH:mm');
-                let tempPost = clima.hourly.apparent_temperature.shift();
-                let precipPost = clima.hourly.precipitation.shift();
-                // console.log(idx, horaPost, tempPost, precipPost);
-                climasPosteriores.innerHTML += listado(idx, horaPost, tempPost, precipPost); 
-            }
-        });
-    });
-}
-
-
-
-/////////////////////////////////////////////
-// Todo junto
 
 function recorrerAPI() {
     fetch(apiWeather)
     .then(response => response.json()) 
     .then(clima => {
-        let fechaActualAPI = clima.hourly.time.filter(fechaActualAPI => fechaActualAPI == fechaActual);
-        console.log(fechaActualAPI);
-        let horaActual = moment(fechaActualAPI).format('MM-DD HH:mm');
-        console.log(horaActual);
-        let tempActual = clima.hourly.apparent_temperature.shift();
-        let precipActual = clima.hourly.precipitation.shift();
-        // console.log(horaActual, tempActual, precipActual);
-        climaActual.innerHTML += listado(0, horaActual, tempActual, precipActual);
+        clima.hourly.time.forEach((dato, idx) => {
+            if (clima.hourly.time[idx] >= fechaActual) {
+                    let fechaActualAPI = clima.hourly.time[idx];
+                    // let horaActual = moment(fechaActualAPI).format('MM-DD HH:mm');
+                    let tempActualAPI = clima.hourly.apparent_temperature[idx];
+                    let precipActualAPI = clima.hourly.precipitation[idx];
+                // 1° caso
+                if (clima.hourly.time[idx] == fechaActual) climaActual.innerHTML += imprimir(0, fechaActualAPI, tempActualAPI, precipActualAPI);
+                else climasPosteriores.innerHTML += imprimir(0, fechaActualAPI, tempActualAPI, precipActualAPI);
+            }
+        }) 
     });
 }
 
 
 
 // Llamado a funciones
-imprimirActual();
-imprimirPosteriores();
+recorrerAPI();
