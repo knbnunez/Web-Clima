cc978f499d2c8ae42f34fa3efd99c578243752f2
knbnunez <knbnunez@gmail.com>
2022-11-11 18:56:49 -0300

    Última versión v2 :p

--- api_weather_V.1.0.js
+++ api_weather_V.1.0.js
@@ -1,13 +1,8 @@
-// const imprimir = require('imprimir.js');
-
-
-
 
 function imprimirCards(diaHoraActual, temperaturaAmbiente, sensacionTermica, precipitacionFormatted, velocidadViento) {
     precipitacionActualIconoID.innerHTML = `<img src="${precipitacionFormatted[0]}" style="width: 60%; margin-top: 15px">`;
     temperaturaAmbienteActualID.innerHTML = `${temperaturaAmbiente} °C`;
     precipitacionActualTextoID.innerHTML = `${precipitacionFormatted[1]}`;
-    // -
     diaHoraActualID.innerHTML = `${diaHoraActual}`;
     // 
     sensacionTermicaActualID.innerHTML = `<h4>${sensacionTermica} °C</h4>`;
@@ -25,24 +20,20 @@ function imprimirTabla(fechaFormatted, precipitacionFormatted, temperaturaAmbien
             <td><h5>${precipitacionFormatted[1]}</h6></td>
             <td><h5>${sensacionTermica} °C</h5></td>
             <td><h5>${velocidadViento} Km/h</h5></td>
-        </td>
+        </tr>
     `;
 }
 
 function imprimirDiasSiguientes(avgPrecipitacion, avgTemperaturaAmbiente, avgSensacionTermica, avgVelocidadViento, dia) {
-    console.log('Entré a imprimir días siguientes');
     ult_row.innerHTML += `
-        <div class="col cards-siguientes">
+        <div class="col col-siguientes">
             <div><img src="${avgPrecipitacion[0]}" style="width: 40%"></div>
             <div><p>${avgTemperaturaAmbiente} °C</p></div>
             <div><p>${avgPrecipitacion[1]}</p></div>
-            <!-- Linea divisora -->
             <div><img src="sources/sensacion-termica.png" style="width: 40%"><span>${avgSensacionTermica} °C</span></div>
             <div><p>Sensación térmica</p></div>
-            <!-- Linea divisora -->
             <div><img src="sources/velocidad-viento.png" style="width: 40%"><span>${avgVelocidadViento} Km/h</span></div>
             <div><p>Velocidad del viento</p></div>
-            <!-- Linea divisora -->
             <div><img src="sources/ubicacion.png" style="width: 5%">  Ushuaia, TDF</div>
             <div><img src="sources/reloj.png" style="width: 5%"> ${dia}</div>
         </div>
@@ -56,22 +47,14 @@ fechaActual = moment(fechaOrigen).format('YYYY-MM-DD\THH:00');
 let diaActual = moment(fechaOrigen).format('MM-DD');
 let horaActual = moment(fechaOrigen).format('HH:mm');
 let diaHoraActual = moment(fechaOrigen).format('YYYY-MM-DD HH:mm');
-console.log(diaHoraActual);
 
 const apiWeather = 'https://api.open-meteo.com/v1/forecast?latitude=-54.82&longitude=-68.36&hourly=temperature_2m,apparent_temperature,precipitation,windspeed_10m&timezone=America%2FSao_Paulo';
-
-// Datos sin procesar
-fetch(apiWeather)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-    });
 
 
 function formatearPrecipitacion(precipitacion) {
     if (precipitacion == 0) return ["sources/despejado.png", "Despejado"];
     else if (precipitacion < 2) return ["sources/lluvias-debiles.png", "Lluvias débiles"];
-    else if (precipitacion <= 15) return ["sources/lluvia.png", "lluvia"];
+    else if (precipitacion <= 15) return ["sources/lluvia.png", "Lluvia"];
     else if (precipitacion <= 30) return ["sources/lluvias-fuertes.png", "Lluvias fuertes"]; 
     else if (precipitacion <= 60) return ["sources/lluvias-muy-fuertes.png", "Lluvias muy fuertes"];
     else return ["sources/lluvias-torrenciales.png", "Lluvias torrenciales"];
@@ -111,21 +94,20 @@ function consultaAPI() {
                     imprimirTabla(fechaFormatted, precipitacionFormatted, temperaturaAmbiente, sensacionTermica, velocidadViento);
                 }//climaHorasPosteriores.innerHTML += imprimir(idx, fechaFormatted, temperaturaAmbiente, sensacionTermica, precipitacionFormatted, velocidadViento);
                 else {
-                    diaPosterior = {
+                    horaDiaPosterior = {
                         time: clima.hourly.time[idx],
                         temperature_2m: clima.hourly.temperature_2m[idx],
                         apparent_temperature: clima.hourly.apparent_temperature[idx],
                         precipitation: clima.hourly.precipitation[idx],
                         windspeed_10m: clima.hourly.windspeed_10m[idx]
                     }
-                    posteriores.push(diaPosterior);
+                    posteriores.push(horaDiaPosterior);
                 }
             }
         }) 
 
         // Imprimir posteriores
         for (let i = 0; i < 6; i++) {
-            let fecha = "";
             let avgTemperaturaAmbiente = new Number();
             let avgSensacionTermica = new Number();
             let avgPrecipitacion = new Number();
@@ -143,8 +125,7 @@ function consultaAPI() {
                     avgPrecipitacion =  (avgPrecipitacion/24).toFixed(2);
                     avgVelocidadViento =  (avgVelocidadViento/24).toFixed(2);
                 }
-            }
-            // climaDiasPosteriores.innerHTML += imprimir(i, dia, avgTemperaturaAmbiente, avgSensacionTermica, avgPrecipitacion, avgVelocidadViento);    
+            } 
             avgPrecipitacion = formatearPrecipitacion(avgPrecipitacion);
             imprimirDiasSiguientes(avgPrecipitacion, avgTemperaturaAmbiente, avgSensacionTermica, avgVelocidadViento, dia);
         }
